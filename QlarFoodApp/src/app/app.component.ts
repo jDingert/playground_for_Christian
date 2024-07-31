2918736239f5433133d35eec6355849938517434
Christian Weitsch <c.weitsch@schenckprocess.com>
2024-07-31 15:02:09 +0200

    SCSS aufgeräumt und Tabelle hinzugefügt

--- QlarFoodApp/src/app/app.component.ts
+++ QlarFoodApp/src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ButtonComponent } from "./button/button.component";
-import { SearchbarComponent } from './searchbar/searchbar.component';
-import { SidebarComponent } from "./sidebar/sidebar.component";
-import { FoodChooseScreenComponent } from "./food-choose-screen/food-choose-screen.component";
+import { ButtonComponent } from "./components/button/button.component";
+import { SearchbarComponent } from './components/searchbar/searchbar.component';
+import { SidebarComponent } from "./components/sidebar/sidebar.component";
+import { FoodChooseScreenComponent } from "./components/food-choose-screen/food-choose-screen.component";
+import { DataTableComponent } from "./components/data-table/data-table.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonComponent, SearchbarComponent, SidebarComponent, FoodChooseScreenComponent],
+  imports: [RouterOutlet, ButtonComponent, SearchbarComponent, SidebarComponent, FoodChooseScreenComponent, DataTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
