ea0bf8f871589ee756c95b6a3b8b03b72a94a45a
Julia Dingert <j.dingert@schenckprocess.com>
2024-08-05 16:46:21 +0200

    ngfor für Sidebar

--- QlarFoodApp/src/app/components/sidebar/sidebar.component.ts
+++ QlarFoodApp/src/app/components/sidebar/sidebar.component.ts
@@ -1,15 +1,14 @@
 import { Component, Input } from '@angular/core';
-import { ButtonComponent } from "../button/button.component";
+import { ButtonComponent } from '../button/button.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, NgFor],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrl: './sidebar.component.scss',
 })
-
 export class SidebarComponent {
-
-  @Input() text: string = 'Default';
+  @Input() items: string[] = [];
 }
