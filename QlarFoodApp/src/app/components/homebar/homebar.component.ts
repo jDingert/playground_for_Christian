e7ae5d8e16607c3035bbcc0c63995e63009ff9f4
Christian Weitsch <c.weitsch@schenckprocess.com>
2024-08-06 08:50:37 +0200

    Homebar mit NgFor erstellt

--- QlarFoodApp/src/app/components/homebar/homebar.component.ts
+++ QlarFoodApp/src/app/components/homebar/homebar.component.ts
@@ -1,15 +1,16 @@
 import { Component, Input } from '@angular/core';
 import { ButtonComponent } from "../button/button.component";
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-homebar',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, NgFor],
   templateUrl: './homebar.component.html',
   styleUrl: './homebar.component.scss'
 })
 export class HomebarComponent {
 
-  @Input() label: string = 'Default';
+  @Input() items: string[] = [];
 
 }
